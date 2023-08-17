f4831b9ee385fc080ec0ff19439dcc922d1cbb7b
roritopra <1193102598@u.icesi.edu.co>
2023-08-17 09:40:47 -0500

    [UPDATE] Ended landing page

--- landing-react/src/App.jsx
+++ landing-react/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import { Header } from "./components/Header"
 import { Body } from "./components/Body"
+import { Footer } from "./components/Footer"
 
 export function App() {
     return(
         <>
             <Header />
             <Body />
+            <hr></hr>
+            <Footer />
         </>
     )
 }
