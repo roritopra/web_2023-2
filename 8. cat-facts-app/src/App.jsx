8d43979f79eeafa2eb8624bba65d57f2d98b470d
roritopra <1193102598@u.icesi.edu.co>
2023-10-01 18:50:20 -0500

    [UPDATE] Fixed function for reload api

--- 8. cat-facts-app/src/App.jsx
+++ 8. cat-facts-app/src/App.jsx
@@ -1,24 +1,26 @@
 import { RenderFacts } from './components/RenderFacts';
 import { RenderCats } from './components/RenderCats';
 import './index.css';
+import React from 'react';
 
 function App() {
-
   const reloadApi = () => {
-    window.location.reload();
+    setReloadKey(prevKey => !prevKey); 
   };
+
+  const [reloadKey, setReloadKey] = React.useState(false); 
 
   return (
     <main className='grid place-content-center min-h-screen max-h-screen'>
       <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md border border-gray-700">
         <div className="relative mx-4 max-h-80 -mt-6 h-full overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
-          <RenderCats />
+          <RenderCats key={reloadKey} /> 
         </div>
         <div className="p-6">
-          <RenderFacts />
+          <RenderFacts key={reloadKey} />
         </div>
         <div className="p-6 pt-0">
-          <button onClick={reloadApi} data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] disabled:opacity-50 disabled:shadow-none">
+          <button onClick={reloadApi} data-ripple-light="true" type="button" className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-gray-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] disabled:opacity-50 disabled:shadow-none">
             Try again
           </button>
         </div>
