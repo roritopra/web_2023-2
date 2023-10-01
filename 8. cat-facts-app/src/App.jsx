16bd782435baf83c4f0f8ce555a7760111f3be9c
roritopra <1193102598@u.icesi.edu.co>
2023-10-01 18:12:13 -0500

    [ADD] Added background with finisher

--- 8. cat-facts-app/src/App.jsx
+++ 8. cat-facts-app/src/App.jsx
@@ -5,7 +5,7 @@ import './index.css';
 function App() {
 
   return (
-    <main className='grid place-content-center min-h-screen max-h-screen bg-[#111827]'>
+    <main className='grid place-content-center min-h-screen max-h-screen'>
       <div className="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md border border-gray-700">
         <div className="relative mx-4 max-h-80 -mt-6 h-full overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
           <RenderCats />
