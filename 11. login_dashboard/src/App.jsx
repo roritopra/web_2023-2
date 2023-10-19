e3ddca5217bfa9e8f52172aaadc00dc444b1f0ac
roritopra <1193102598@u.icesi.edu.co>
2023-10-18 21:52:11 -0500

    [ADD] Added AOS

--- 11. login_dashboard/src/App.jsx
+++ 11. login_dashboard/src/App.jsx
@@ -1,8 +1,12 @@
+import LoginPage from "./pages/LoginPage"
+
 function App() {
 
   return (
     <>
       <h1 className='text-xl bg-red-500 font-bold underline'>HOLA ES UN APRUEBA</h1>
+
+      <LoginPage />
     </>
   )
 }
