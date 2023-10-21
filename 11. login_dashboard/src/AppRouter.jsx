f77e4a47ecdc83c13e5d269e5a43235a1d7b4e9c
roritopra <1193102598@u.icesi.edu.co>
2023-10-21 00:52:27 -0500

    Checkpoint

--- 11. login_dashboard/src/AppRouter.jsx
+++ 11. login_dashboard/src/AppRouter.jsx
@@ -3,6 +3,7 @@ import { LandingPage } from "./pages/LandingPage";
 import { LoginPage } from "./pages/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage";
 import { PageNotFound } from "./pages/PageNotFound";
+import { LaunchesPage } from "./pages/LaunchesPage";
 
 export function AppRouter() {
 
@@ -13,6 +14,7 @@ export function AppRouter() {
         <Route path="/home" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/launches" element={<LaunchesPage />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </main>
---
+++ 11. login_dashboard/src/pages/LaunchesPage.jsx
@@ -0,0 +1,64 @@
+import Menu from "../components/Menu"
+import './launches.css'
+
+export function LaunchesPage() {
+  
+
+
+  return (
+    <>
+    <Menu />
+      <main className="bg-black">
+        <header
+          className="h-screen overflow-hidden relative z-20"
+        >
+          <div className="relative h-full w-full">
+            <img
+              src="bg-launches.jpg"
+              alt="image 1"
+              className="h-full w-full object-cover"
+            />
+            <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/50">
+              <div className="w-3/4 text-center md:w-2/4">
+                <h1
+                  data-aos="fade-up"
+                  data-aos-duration="1500"
+                  data-aos-delay="800"
+                  className="mb-4 text-3xl md:text-4xl lg:text-7xl text-white font-roobert font-bold"
+                >
+                  LAUNCHES
+                </h1>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <section className="grid gap-20 sm:grid-cols-2 lg:grid-cols-3 mt-32">
+          <div className="flex flex-col items-center gap-1">
+            <h6 className="text-9xl font-roobert text-white">273</h6>
+            <p className="text-white text-base">TOTAL LAUNCHES</p>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <h6 className="text-9xl font-roobert text-white">236</h6>
+            <p className="text-white text-base">TOTAL LANDINGS</p>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <h6 className="text-9xl font-roobert text-white">208</h6>
+            <p className="text-white text-base">TOTAL REFLIGHTS</p>
+          </div>
+        </section>
+
+
+
+<div id="cards">
+  <div className="card">
+    <div className="card-content">
+    </div>
+  </div>
+</div>
+
+      </main>
+    </>
+  )
+}
+
