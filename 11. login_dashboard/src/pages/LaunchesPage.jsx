67c31908f9b9f5a386d77c4f300be9f5d0989078
roritopra <1193102598@u.icesi.edu.co>
2023-10-21 16:15:21 -0500

    [FIX] Fixed cards launnches

--- 11. login_dashboard/src/pages/LaunchesPage.jsx
+++ 11. login_dashboard/src/pages/LaunchesPage.jsx
@@ -2,7 +2,6 @@ import Menu from "../components/Menu";
 import "./launches.css";
 
 export function LaunchesPage() {
-  
   document.getElementById("cards").onmousemove = (e) => {
     for (const card of document.getElementsByClassName("card")) {
       const rect = card.getBoundingClientRect(),
@@ -55,18 +54,104 @@ export function LaunchesPage() {
           </div>
         </section>
 
-        <div
-          id="cards"
-          className="grid mx-14 gap-7 sm:grid-cols-2 lg:grid-cols-3 mt-32 "
-        >
+        <div id="cards" className="grid mx-4 gap-3 mt-32 sm:grid-cols-2 lg:grid-cols-3 lg:mx-14">
+
           <div className="card">
-            <div className="card-content"></div>
+            <div className="card-content">
+              <div className="card-image">
+                <i className="fa-duotone fa-apartment"></i>
+              </div>
+              <div className="card-info-wrapper">
+                <div className="card-info">
+                  <i className="fa-duotone fa-apartment"></i>
+                  <div className="card-info-title">
+                    <h3>Apartments</h3>
+                    <h4>Places to be apart. Wait, what?</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+
           </div>
           <div className="card">
-            <div className="card-content"></div>
+            <div className="card-content">
+              <div className="card-image">
+                <i className="fa-duotone fa-unicorn"></i>
+              </div>
+              <div className="card-info-wrapper">
+                <div className="card-info">
+                  <i className="fa-duotone fa-unicorn"></i>
+                  <div className="card-info-title">
+                    <h3>Unicorns</h3>
+                    <h4>A single corn. Er, I mean horn.</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="card">
-            <div className="card-content"></div>
+            <div className="card-content">
+              <div className="card-image">
+                <i className="fa-duotone fa-blender-phone"></i>
+              </div>
+              <div className="card-info-wrapper">
+                <div className="card-info">
+                  <i className="fa-duotone fa-blender-phone"></i>
+                  <div className="card-info-title">
+                    <h3>Blender Phones</h3>
+                    <h4>These absolutely deserve to exist.</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-content">
+              <div className="card-image">
+                <i className="fa-duotone fa-person-to-portal"></i>
+              </div>
+              <div className="card-info-wrapper">
+                <div className="card-info">
+                  <i className="fa-duotone fa-person-to-portal"></i>
+                  <div className="card-info-title">
+                    <h3>Adios</h3>
+                    <h4>See you...</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-content">
+              <div className="card-image">
+                <i className="fa-duotone fa-person-from-portal"></i>
+              </div>
+              <div className="card-info-wrapper">
+                <div className="card-info">
+                  <i className="fa-duotone fa-person-from-portal"></i>
+                  <div className="card-info-title">
+                    <h3>I mean hello</h3>
+                    <h4>...over here.</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <div className="card-content">
+              <div className="card-image">
+                <i className="fa-duotone fa-otter"></i>
+              </div>
+              <div className="card-info-wrapper">
+                <div className="card-info">
+                  <i className="fa-duotone fa-otter"></i>
+                  <div className="card-info-title">
+                    <h3>Otters</h3>
+                    <h4>Look at me, imma cute lil fella.</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
