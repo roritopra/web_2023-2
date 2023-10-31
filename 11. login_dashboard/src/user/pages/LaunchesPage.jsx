41d7b10aa596ec05fd892954ee89f04897c0d3cd
roritopra <1193102598@u.icesi.edu.co>
2023-10-31 13:38:08 -0500

    [UPDAT] Updated responsive por pagination

--- 11. login_dashboard/src/user/pages/LaunchesPage.jsx
+++ 11. login_dashboard/src/user/pages/LaunchesPage.jsx
@@ -99,71 +99,6 @@ export function LaunchesPage() {
           </button>
         </aside>
 
-        <ReactPaginate
-          previousLabel={
-            <div className="flex items-center justify-center px-3 h-8 ml-0 leading-tight text-white bg-[#434343] border border-[#6B7280] rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-              <span className="sr-only">Previous</span>
-              <svg
-                className="w-2.5 h-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 1 1 5l4 4"
-                />
-              </svg>
-            </div>
-          }
-          nextLabel={
-            <div className="flex items-center justify-center px-3 h-8 leading-tight text-white bg-[#434343] border border-[#6B7280] rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-              <span className="sr-only">Next</span>
-              <svg
-                className="w-2.5 h-2.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 6 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="m1 9 4-4-4-4"
-                />
-              </svg>
-            </div>
-          }
-          breakLabel={
-            <div className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-[#434343] border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-              ...
-            </div>
-          }
-          pageCount={Math.ceil(launches.length / launchesPerPage)}
-          marginPagesDisplayed={2}
-          pageRangeDisplayed={5}
-          onPageChange={handlePageClick}
-          containerClassName={"flex items-center -space-x-px h-8 text-sm"}
-          pageClassName={""}
-          pageLinkClassName={
-            "flex items-center justify-center px-3 h-8 leading-tight text-white bg-[#434343] border border-[#6B7280] hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          }
-          breakClassName={""}
-          previousClassName={""}
-          nextClassName={""}
-          previousLinkClassName={""}
-          nextLinkClassName={""}
-          activeClassName={
-            "z-10 flex items-center justify-center h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-          }
-        />
-
         <section
           id="cards"
           className="grid mx-2 gap-3 mt-32 sm:grid-cols-2 lg:grid-cols-3 lg:mx-14"
@@ -203,6 +138,73 @@ export function LaunchesPage() {
             </article>
           ))}
         </section>
+
+        <nav className="flex justify-center items-center py-20">
+          <ReactPaginate
+            previousLabel={
+              <div className="flex items-center justify-center px-4 h-10 ml-0 leading-tight text-white bg-[#434343] border border-[#6B7280] rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                <span className="sr-only">Previous</span>
+                <svg
+                  className="w-2.5 h-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 1 1 5l4 4"
+                  />
+                </svg>
+              </div>
+            }
+            nextLabel={
+              <div className="flex items-center justify-center px-4 h-10 leading-tight text-white bg-[#434343] border border-[#6B7280] rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                <span className="sr-only">Next</span>
+                <svg
+                  className="w-2.5 h-2.5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+              </div>
+            }
+            breakLabel={
+              <div className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-[#434343] border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+                ...
+              </div>
+            }
+            pageCount={Math.ceil(launches.length / launchesPerPage)}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={5}
+            onPageChange={handlePageClick}
+            containerClassName={"flex items-center -space-x-px h-10 text-sm"}
+            pageClassName={"hidden sm:block"} // Oculta los números de página en pantallas pequeñas (responsive)
+            pageLinkClassName={
+              "flex items-center justify-center font-roobert px-4 h-10 leading-tight text-white bg-[#434343] border border-[#6B7280] hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+            }
+            breakClassName={"hidden sm:block"} // Oculta los puntos suspensivos en pantallas pequeñas (responsive)
+            previousClassName={""}
+            nextClassName={""}
+            previousLinkClassName={""}
+            nextLinkClassName={""}
+            activeClassName={
+              "z-50 flex items-center justify-center bg-blue-60 h-10 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+            }
+          />
+        </nav>
       </main>
     </>
   );
