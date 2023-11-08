b533613afee233ca457c3affe5cdf3b53c4667b9
roritopra <1193102598@u.icesi.edu.co>
2023-11-07 22:47:40 -0500

    [UPDATE] Update styles about page first part

--- 11. login_dashboard/src/user/pages/AboutPage.jsx
+++ 11. login_dashboard/src/user/pages/AboutPage.jsx
@@ -3,13 +3,27 @@ import "./AboutPage.css";
 export function AboutPage() {
   return (
     <>
-      <main className="bg-black pt-20 flex justify-center">
-        <div className="container">
-          <div className="sun"></div>
-          <div className="mercury"></div>
-          <div className="earth">
-            <div className="moon"></div>
+      <main className="bg-black px-5 py-20">
+        <div className="flex justify-center mb-16">
+          <div className="main-container">
+            <div className="sun"></div>
+            <div className="mercury"></div>
+            <div className="earth">
+              <div className="moon"></div>
+            </div>
           </div>
+        </div>
+        <div className="flex justify-center flex-col items-center">
+          <h1 className="text-white font-din text-2xl mb-5">
+            FIND YOUR FUTURE
+          </h1>
+          <p className="text-white text-sm font-roobertLight">
+            Welcome to SpaceXplore, your one-stop destination for all things
+            SpaceX! We are dedicated to bringing the excitement of space
+            exploration right to your fingertips. With SpaceXplore, you can
+            watch live launches as they happen, keeping you up-to-date with the
+            latest missions and groundbreaking moments in space history.
+          </p>
         </div>
       </main>
     </>
